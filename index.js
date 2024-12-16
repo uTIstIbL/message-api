ecd9bcd8d682fc8c8ac0332d0739d35d839e5ff9
AaronKao <aaronkao88@gmail.com>
2024-12-16 18:35:49 +0800

    新增: 高流量控制, 發送訊息API, 更改套件為 Mongoose

--- index.js
+++ index.js
@@ -2,45 +2,60 @@ const express = require('express');
 const morgan = require('morgan')
 const app = express();
 const PORT = 3000;
-const db = require('./config/db')
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length
+const mongoose = require("mongoose");
+const { config } = require('dotenv')
 
 // import package.json
 const pkg = require('./package.json')
 
 // import api
-const apiMessage = require('./routes/message')
+const apiMessage = require('./routes/message');
 
-// 連接資料庫
+if(!cluster.isMaster){
+    console.log("最大流量啟動")
+
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    
+    cluster.on('exit', () => {
+        console.log(`工作流量 過載. 分給一個新的伺服器...`);
+        cluster.fork();
+    });
+} else {
 async function run() {
     try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await db.client.connect();
-      // Send a ping to confirm a successful connection
-      await db.client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await db.client.close();
+      await mongoose.connect("mongodb+srv://messengerAdmin:messengerAdmin@message-api.qx3ni.mongodb.net/?retryWrites=true&w=majority&appName=message-api");
+      console.log("資料庫連線成功");
+    } catch (error) {
+      
+      console.error(error, "資料庫連線失敗")
     }
   }
-run().catch(console.dir);
+  run()
+  
+  
+  // 中介軟體使用
+  app.use(morgan('Aaron'))
+  app.use(morgan(':method :url :response-time'));
+  
+  // 引入api
+  config();
+  app.use('/message', apiMessage)
+  
+  app.get('/', (req,res) => {
+      return res.status(200).json({
+          "name":pkg.name,
+          "version":pkg.version
+      })
+  })
+  
+  
+  app.listen(PORT, () => {
+      console.log(`伺服器已啟動在 http://localhost:${PORT}`);
+      
+  })
+}  
 
-// 中介軟體使用
-app.use(morgan('Aaron'))
-app.use(morgan(':method :url :response-time'));
-
-// 引入api
-app.use('/message', apiMessage)
-
-app.get('/', (req,res) => {
-    return res.status(200).json({
-        "name":pkg.name,
-        "version":pkg.version
-    })
-})
-
-
-app.listen(PORT, () => {
-    console.log(`伺服器已啟動在 http://localhost:${PORT}`);
-    
-})
