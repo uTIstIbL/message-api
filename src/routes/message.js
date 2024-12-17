30b22fcf6104e39e0ef33d034df3a6882771e7d2
AaronKao <aaronkao88@gmail.com>
2024-12-17 12:49:51 +0800

    新增及修復: 訊息發送及接收功能

--- src/routes/message.js
+++ src/routes/message.js
@@ -6,28 +6,66 @@ const Messenger = require('../models/message')
 
 
 router.get('/', async (req,res) => {
+    try {
+        const messageFind = await Messenger.find({})
+    
+        return res.status(200).json({
+            status:200,
+            data:{
+                messageFind
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            data:{
+                error: "操作失敗：伺服器錯誤"
+            }
+        })
+    }
 })
 
 router.post('/', async (req,res) => {
-    const sendMessenger = { messenger: req.body.messenger }
-
-    await Messenger.create({ sendMessenger })
-
-    return res.status(200).json({
-        status: 200,
-        data:{
-            sendMessenger
+    try {
+        const sendMessenger = { 
+            messenger: req.body.messenger 
         }
-    })
+    
+        const Message = await Messenger.create({ message: req.body.messenger })
+
+        return res.status(200).json({
+            status: 200,
+            data:{
+                Message: Message
+            }
+        })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({
+            status: 500,
+            data:{
+                error: "操作失敗：伺服器錯誤"
+            }
+        })
+    }
     
 })
 
 router.post('/onetomore', (req,res) => {
-
+    try { 
+                
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            data:{
+                error: "操作失敗：伺服器錯誤"
+            }
+        })
+    }
 })
 
 router.put('/onnlinestatus', (req,res) => {
-
+    
 })
 
 module.exports = router
